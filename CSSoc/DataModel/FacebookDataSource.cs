d4693301bcb5cdc30e79a1b1a5ae88c31c2c1674
Pez Cuckow <email@pezcuckow.com>
2013-10-05 14:07:00 +0100

    Upload all the stuff

---
+++ CSSoc/DataModel/FacebookDataSource.cs
@@ -0,0 +1,152 @@
+﻿using Facebook;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The data model defined by this file serves as a representative example of a strongly-typed
+// model.  The property names chosen coincide with data bindings in the standard item templates.
+//
+// Applications may use this model as a starting point and build on it, or discard it entirely and
+// replace it with something appropriate to their needs. If using this model, you might improve app 
+// responsiveness by initiating the data loading task in the code behind for App.xaml when the app 
+// is first launched.
+
+namespace CSSoc.Data
+{
+    public class FacebookPage
+    {
+        public string Id { get; private set; }
+        public ObservableCollection<FacebookEvent> Events { get; private set; }
+
+        public FacebookPage(dynamic pageInfo)
+        {
+            this.Id = pageInfo.id;
+
+            this.Events = new ObservableCollection<FacebookEvent>();
+            foreach(dynamic eventInfo in pageInfo.events.data) {
+                this.Events.Add(new FacebookEvent(eventInfo));
+            }
+            
+        }
+    }
+
+    /// <summary>
+    /// Generic item data model.
+    /// </summary>
+    public class FacebookEvent
+    {
+        public FacebookEvent(dynamic eventInfo)
+        {
+            this.Id = eventInfo.id;
+            this.AttendingCount = eventInfo.attending.data.Count;
+            this.CoverImage = eventInfo.cover.source;
+            this.Description = eventInfo.description;
+            this.EndTime = DateTime.Parse(eventInfo.end_time);
+            this.StartTime = DateTime.Parse(eventInfo.start_time);
+            this.IsDateOnly = eventInfo.is_date_only;
+            this.MaybeCount = eventInfo.maybe.data.Count;
+            this.Location = eventInfo.location;
+            this.Name = eventInfo.name;
+            // cover,description,location,attending.fields(id),maybe.fields(id),start_time,end_time,name,is_date_only
+        }
+
+        public string Id { get; private set; }
+        public string Location { get; private set; }
+        public string Name { get; private set; }
+        public int AttendingCount { get; private set; }
+        public int MaybeCount { get; private set; }
+        public string CoverImage { get; private set; }    
+        public string Description { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public DateTime StartTime { get; private set; }
+        public Boolean IsDateOnly { get; private set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+    /// <summary>
+    /// Creates a collection of groups and items with content read from a static json file.
+    /// 
+    /// SampleDataSource initializes with data read from a static json file included in the 
+    /// project.  This provides sample data at both design-time and run-time.
+    /// </summary>
+    public sealed class FacebookDataSource
+    {
+        private static FacebookDataSource _dataSource = null;
+
+        static FacebookDataSource()
+        {
+            _dataSource = new FacebookDataSource();
+        }
+
+        public static async Task<IEnumerable<FacebookEvent>> GetEventsAsync()
+        {
+            await _dataSource.GetFacebookDataAsync();
+
+            return _dataSource.Events;
+        }
+
+        private FacebookPage _page = null;
+        public ObservableCollection<FacebookEvent> Events
+        {
+            get {
+                if (this._page == null) return null;
+
+                return this._page.Events;
+            }
+        }
+
+        public static async Task<FacebookEvent> GetEventAsync(string id)
+        {
+            await _dataSource.GetFacebookDataAsync();
+            // Simple linear search is acceptable for small data sets
+            var matches = _dataSource.Events.Where((e) => e.Id.Equals(id));
+            return matches.First();
+        }
+
+        /*public static async Task<SampleDataItem> GetItemAsync(string uniqueId)
+        {
+            await _sampleDataSource.GetSampleDataAsync();
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.Groups.SelectMany(group => group.Items).Where((item) => item.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }*/
+
+        FacebookClient _fb = new FacebookClient();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private async Task GetFacebookDataAsync()
+        {
+            if (this.Events != null && this.Events.Count != 0)
+                return;
+
+            dynamic facebookReply = await _fb.GetTaskAsync("oauth/access_token", new
+            {
+                client_id = "378133422317406",
+                client_secret = "b8f6ebf52f9fd41cb4e19583f6cce00b",
+                grant_type = "client_credentials"
+            });
+            _fb.AccessToken = facebookReply.access_token;
+
+            dynamic parameters = new ExpandoObject();
+            parameters.fields = "events.fields(cover,description,location,attending.fields(id),maybe.fields(id),start_time,end_time,name,is_date_only),albums.limit(10).fields(name,photos.limit(60).fields(source))";
+            dynamic cssocManData = await _fb.GetTaskAsync("cssoc.man", parameters);
+
+            this._page = new FacebookPage(cssocManData);
+        }
+    }
+}
---
+++ CSSoc/HubPage.xaml.cs
@@ -0,0 +1,144 @@
+﻿using CSSoc.Common;
+using CSSoc.Data;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Facebook;
+
+// The Hub Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=??????
+
+namespace CSSoc
+{
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class HubPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        FacebookClient _fb = new FacebookClient();
+        public HubPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private async void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            IEnumerable<FacebookEvent> events = await FacebookDataSource.GetEventsAsync();
+        
+            int upcomingEventCount = (events.Count() - 1);
+
+            this.DefaultViewModel["UpcomingEvents"] = events.Take(upcomingEventCount);
+
+            // The last item in the list if the first event
+            this.DefaultViewModel["NextEvent"] = events.Skip(upcomingEventCount).Take(1);
+            /*dynamic res = await _fb.GetTaskAsync("oauth/access_token", new
+            {
+                client_id = "378133422317406",
+                client_secret = "b8f6ebf52f9fd41cb4e19583f6cce00b",
+                grant_type = "client_credentials"
+            });
+            _fb.AccessToken = res.access_token;
+
+            dynamic parameters = new ExpandoObject();
+            parameters.fields = "events.fields(cover,description,location,attending.fields(id),maybe.fields(id),start_time,end_time,name,is_date_only),albums.limit(10).fields(name,photos.limit(60).fields(source))";
+            dynamic result = await _fb.GetTaskAsync("cssoc.man", parameters);
+
+            Facebook.JsonArray events = result.events.data;
+
+            //dynamic nextEvent = events[0];
+            this.DefaultViewModel["NextEvent"] = new SampleDataItem("main", nextEvent.name, nextEvent.start_time, nextEvent.cover.source, nextEvent.description, "Content");*/
+        }
+
+        /// <summary>
+        /// Invoked when a HubSection header is clicked.
+        /// </summary>
+        /// <param name="sender">The Hub that contains the HubSection whose header was clicked.</param>
+        /// <param name="e">Event data that describes how the click was initiated.</param>
+        /*void Hub_SectionHeaderClick(object sender, HubSectionHeaderClickEventArgs e)
+        {
+            HubSection section = e.Section;
+            var fbevent = section.DataContext;
+            this.Frame.Navigate(typeof(SectionPage), ((FacebookEvent)fbevent).Id);
+        }*/
+
+        /// <summary>
+        /// Invoked when an item within a section is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView or ListView
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(ItemPage), itemId);
+        }
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
